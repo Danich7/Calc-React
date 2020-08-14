b22cc66c5a3888836e683c48a1f52c3f494030a8
Danich7 <55358164+Danich7@users.noreply.github.com>
2020-08-15 00:37:19 +0300

    Add files via upload

---
+++ precache-manifest.454e0169dcab8221cdf1c237ed3f98ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f33e5f94f644f5cdb02f36f07c45f7",
+    "url": "/Calc-React/index.html"
+  },
+  {
+    "revision": "c709434e695c8f241ae5",
+    "url": "/Calc-React/static/css/main.d41691d1.chunk.css"
+  },
+  {
+    "revision": "ab12b72ec6cd5010e57a",
+    "url": "/Calc-React/static/js/2.42df7b18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calc-React/static/js/2.42df7b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c709434e695c8f241ae5",
+    "url": "/Calc-React/static/js/main.50f4296f.chunk.js"
+  },
+  {
+    "revision": "5c3a25b21401a8e716b3",
+    "url": "/Calc-React/static/js/runtime-main.fb3eac71.js"
+  }
+]);
